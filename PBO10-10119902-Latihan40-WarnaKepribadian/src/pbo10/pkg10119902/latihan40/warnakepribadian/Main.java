1fcf6b15dccfe662dbd05737f557b0b05f032dfd
muhammadnurkholiq <73203850+muhammadnurkholiq@users.noreply.github.com>
2020-11-08 10:39:14 +0700

    Add files via upload

---
+++ PBO10-10119902-Latihan40-WarnaKepribadian/src/pbo10/pkg10119902/latihan40/warnakepribadian/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10.pkg10119902.latihan40.warnakepribadian;
+
+/**
+ *
+ * @author 
+ * NAMA    : Muhammad Nurkholiq
+ * KELAS   : PBO10K
+ * NIM     : 10119902
+ * Deskripsi Program : Program Ini Berisi Program Untuk Menampilkan Program Warna Kepribadian
+ * 
+ */
+public class Main {
+    public static void main(String[] args) {
+        Kepribadian orang = new Kepribadian();
+        orang.getNama();
+        orang.getWarna();
+        orang.getKepribadian();
+    }
+}
